9d46782cd3c1201a82685556f0bcd2864dba92ab
Alex Baron <thealexbaron@gmail.com>
2016-05-31 09:49:54 -0600

    ugh, commit it

---
+++ http-server.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const http = require('http')
+const fs = require('fs')
+const phpUnserialize = require('php-serialization').unserialize
+
+let server = http.createServer((req, res) => {
+    
+    console.log(req.method);
+    console.log(req.url);
+    if (req.url === '/' && req.method === 'GET') {
+        fs.readFile(__dirname + '/textarea.html', (err, data) => {
+            if (err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+            res.writeHead(200);
+            res.end(data);
+        })
+    }
+    else if (req.method === 'POST') {
+        var postString = '';
+
+        req.on('data', data => {
+            postString += data;
+        })
+
+        req.on('end', () => {
+            try {
+                let jsonRequest = JSON.parse(postString);
+                
+                let base64Buffer = new Buffer(jsonRequest.text, 'base64')
+                let serialized = base64Buffer.toString()
+                
+                if (!(/^i|d|s|b|n|c|a|o|r/).test(serialized)) {
+                    throw Error(serialized + ' does not look like serialized php');
+                }
+
+                let unserialized = phpUnserialize(serialized)
+
+                res.writeHead(200)
+                res.end(JSON.stringify({decoded: unserialized}))
+                
+                return
+            }
+            catch (err) {
+                res.writeHead(500)
+                res.end(JSON.stringify({error: err.message}))
+            }
+        });
+    }
+})
+
+server.listen(3012, () => {
+    console.log("Server listening on: http://localhost:%s", 3012);
+});
